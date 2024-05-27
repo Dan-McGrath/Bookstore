1f478a89764490d0c3d5d2397ac2806aac4908e7
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-27 04:24:44 -0400

    create basic Header

---
+++ frontend/src/components/Layout/Header.jsx
@@ -0,0 +1,12 @@
+import Button from '../Button';
+
+const Header = () => {
+  return (
+    <div className="flex justify-between w-full p-4 bg-darkBrown">
+      <img className="" src="" alt="Logo" />
+      <Button text={'Login'} />
+    </div>
+  );
+};
+
+export default Header;
