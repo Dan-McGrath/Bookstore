c27c326e32aea8361b4a72332b562463ec82c4b8
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-27 04:24:35 -0400

    create basic footer

---
+++ frontend/src/components/Layout/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className="flex items-center justify-center w-full max-h-24 bg-darkBrown min-h-20">
+      <p className="text-center">&copy; 2024 Dan McGrath | All Rights Reserved</p>
+    </div>
+  );
+};
+
+export default Footer;
