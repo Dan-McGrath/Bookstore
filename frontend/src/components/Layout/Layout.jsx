122a9657133e52f5230d890198c3f3c274747654
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-27 04:24:56 -0400

    Add footer and header components

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,10 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 const Layout = () => {
   return (
-    <>
-      <Outlet />
-    </>
+    <div className="flex flex-col h-dvh">
+      <Header />
+      <div className="grow">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
   );
 };
 
