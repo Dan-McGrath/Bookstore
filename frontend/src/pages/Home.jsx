7bdd20e187a96b8e4ce82328bf6a83acb7e4f023
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-27 05:16:38 -0400

    Add basic content to home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
-
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <div className="m-auto my-20 min-w-[61rem] max-w-[35]">
+        <img
+          alt="Cozy room filled with books"
+          src="./images/heroBackground.jpg"
+          className="max-w-[61rem] max-h-[35rem] m-auto rounded-3xl"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
