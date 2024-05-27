8ed3c0e7993208042534234dcb153d58dc7a384b
Dan McGrath <mcgrathd3254@yahoo.com>
2024-05-27 04:23:40 -0400

    Add colors and fonts to theme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        darkBrown: '#593325',
+        beige: '#F2D7B6',
+        white: 'F2F2F2',
+        tan: '#A65729',
+        lightTan: '#D98B48',
+      },
+      fontFamily: {
+        primaryRegular: 'Regular',
+        primaryBold: 'Bold',
+        primarySemiBold: 'SemiBold',
+        primarySemiBoldItalic: 'SemiBoldItalics',
+      },
+    },
   },
   plugins: [],
 };
