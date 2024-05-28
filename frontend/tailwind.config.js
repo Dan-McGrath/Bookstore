3a72ed961a6b290d8468979c1e8609a21ae153b8
Dan-McGrath <mcgrathd3254@yahoo.com>
2024-05-28 04:37:24 -0400

    Update color scheme

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,11 +4,13 @@ export default {
   theme: {
     extend: {
       colors: {
-        darkBrown: '#593325',
+        lightBrown: '#A65437',
         beige: '#F2D7B6',
-        white: 'F2F2F2',
-        tan: '#A65729',
-        lightTan: '#D98B48',
+        white: '#F2F2F2',
+        tan: '#F29544',
+        red: '#D94436',
+        darkGreen: '#012626',
+        darkBrown: '#593325',
       },
       fontFamily: {
         primaryRegular: 'Regular',
