1e94b6ba04564473d678a2f9b28f3f474a0ee055
Dan-McGrath <mcgrathd3254@yahoo.com>
2024-05-28 04:39:24 -0400

    Update color

--- frontend/src/components/Layout/Footer.jsx
+++ frontend/src/components/Layout/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div className="flex items-center justify-center w-full max-h-24 bg-darkBrown min-h-20">
+    <div className="flex items-center justify-center w-full max-h-24 bg-lightBrown min-h-20">
       <p className="text-center">&copy; 2024 Dan McGrath | All Rights Reserved</p>
     </div>
   );
