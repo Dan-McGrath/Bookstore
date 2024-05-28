4650c788586f92cc7bb0d350011b9d44dc36fa2d
Dan-McGrath <mcgrathd3254@yahoo.com>
2024-05-28 05:16:08 -0400

    Add context Providers to main component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { AuthContext } from './store/AuthContext.jsx';
+import { UserContext } from './store/UserContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthContext.Provider value={false}>
+      <UserContext.Provider value={{}}>
+        <App />
+      </UserContext.Provider>
+    </AuthContext.Provider>
   </React.StrictMode>,
 );
