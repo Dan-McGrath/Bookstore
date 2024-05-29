52ad0d7a107bec44b5a5ec3b4fecc8e7098535d8
Dan-McGrath <mcgrathd3254@yahoo.com>
2024-05-29 10:13:58 -0400

    Update home page styling/Add company name and slogan

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,14 +4,18 @@ const Home = () => {
   return (
     <>
       <div>
-        <div className="m-auto my-20">
+        <div className="relative m-auto my-15">
+          <div className="my-4 text-center">
+            <h1 className="text-4xl text-darkGreen font-primarySemiBoldItalic">Cozy Corner</h1>
+            <p className="mt-1 text-xl text-darkGreen font-primarySemiBold">Build your own digital library!</p>
+          </div>
           <img
             alt="Cozy room filled with books"
             src="./images/heroBackground.jpg"
             className="max-w-[20.375rem] max-h-[21.5rem] m-auto rounded-3xl"
           />
         </div>
-        <hr className="w-11/12 mx-auto border-2 rounded-full border-darkGreen" />
+        <hr className="w-11/12 mx-auto mt-8 border-2 rounded-full border-darkGreen" />
       </div>
       <div className="flex flex-col">
         <Testimonials />
@@ -21,3 +25,5 @@ const Home = () => {
 };
 
 export default Home;
+
+// <div className="absolute inset-8 backdrop-sepia-0 bg-white/70 max-w-[20rem] max-h-[10rem] rounded-3xl"></div>
