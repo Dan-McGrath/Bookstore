a7e70f1c7d102ffaa74baef90313138f8a2b3acd
Dan-McGrath <mcgrathd3254@yahoo.com>
2024-05-29 13:02:59 -0400

    Add Auth and login components to router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,17 @@ import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } 
 import Home from './pages/home/Home';
 import Layout from './components/Layout/Layout';
 import ErrorPage from './pages/ErrorPage';
+import DashBoard from './pages/dashboard/DashBoard';
+import Auth from './store/auth/Auth';
+import Login from './store/auth/Login';
 
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
       <Route errorElement={<ErrorPage />}>
         <Route index element={<Home />} />
+        <Route path="login" element={<Login />} />
+        <Route path="library" element={<Auth />}></Route>
       </Route>
     </Route>,
   ),
