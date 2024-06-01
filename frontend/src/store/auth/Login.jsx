710ed1f775cd787ffa95436c32962330abe6c2ae
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-01 05:02:40 -0400

    Create basic login form

--- frontend/src/store/auth/Login.jsx
+++ frontend/src/store/auth/Login.jsx
@@ -1,18 +1,59 @@
+import { useState } from 'react';
 import Button from '../../components/Button';
 import { useAuth } from '../../hooks/useAuth';
 
 const Login = () => {
   const { login } = useAuth();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const onSubmit = async () => {
-    await login({ username: 'admin' });
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    // Temo set up until backend is done
+    if (username === 'user' && password === 'password') {
+      await login({ username });
+    } else {
+      alert('Invalid username or password');
+    }
   };
 
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <Button text="Login" />
-      </form>
+      <div className="flex w-full h-full m-0">
+        <form
+          onSubmit={onSubmit}
+          className="flex flex-col gap-3 p-4 m-auto border-2 border-darkBrown rounded-2xl min-w-36 min-h-36 max-h-64 max-w-64"
+        >
+          <div className="flex flex-col gap-3 grow">
+            <label htmlFor="username" className="sr-only">
+              Username:
+            </label>
+            <input
+              id="username"
+              type="text"
+              placeholder="Username"
+              className="px-2 rounded-2xl text-darkBrown"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+            />
+            <label htmlFor="password" className="sr-only">
+              Password:
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              id="password"
+              className="px-2 rounded-2xl text-darkBrown"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <Button text="Login" type="submit" />
+        </form>
+      </div>
     </>
   );
 };
