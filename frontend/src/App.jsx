e18bfee3c3dcb88106dd3e77c45ab0132cfc115f
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-01 04:19:26 -0400

    refactor to allow AuthProvider to surround routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,32 @@
-import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import Layout from './components/Layout/Layout';
 import ErrorPage from './pages/ErrorPage';
 import DashBoard from './pages/dashboard/DashBoard';
 import Auth from './store/auth/Auth';
 import Login from './store/auth/Login';
-
-const appRouter = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
-      <Route errorElement={<ErrorPage />}>
-        <Route index element={<Home />} />
-        <Route path="login" element={<Login />} />
-        <Route path="library" element={<Auth />}>
-          <Route index element={<DashBoard />} />
-        </Route>
-      </Route>
-    </Route>,
-  ),
-);
+import { AuthProvider } from './hooks/useAuth';
+import SideNav from './components/Layout/SideNav';
 
 const App = () => {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
+          <Route errorElement={<ErrorPage />}>
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+          </Route>
+        </Route>
+        <Route path="/library" element={<Auth />}>
+          <Route errorElement={<ErrorPage />}>
+            <Route index element={<DashBoard />} />
+            <Route element={<SideNav />} />
+          </Route>
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
 };
 
 export default App;
