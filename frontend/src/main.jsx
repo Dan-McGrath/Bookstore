91f368784513acdcccd84527d422026784ee4c75
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-01 04:16:42 -0400

    remove contexts and add BrowserRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { AuthContext } from './store/AuthContext.jsx';
-import { UserContext } from './store/UserContext.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthContext.Provider value={{ isAuthorized: false }}>
-      <UserContext.Provider value={{}}>
-        <App />
-      </UserContext.Provider>
-    </AuthContext.Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 );
