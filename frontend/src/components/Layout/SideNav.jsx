5ae6890bae20953daa72eba0c99697ace435ce56
Dan McGrath <mcgrathd3254@yahoo.com>
2024-06-01 04:17:35 -0400

    create layout after user is logged in

--- frontend/src/components/Layout/SideNav.jsx
+++ frontend/src/components/Layout/SideNav.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 
 const SideNav = () => {
   return (
-    <>
-      <div>Sidenav</div>
-      <Outlet />
-    </>
+    <div className="flex flex-col h-dvh">
+      <Header />
+      <div className="grid grid-cols-12 gap-1 grow">
+        <div className="col-start-2">SideNav</div>
+        <div className="col-span-4 col-start-6 bg-beige">
+          <Outlet />
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 };
-
 export default SideNav;
